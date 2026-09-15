c0d4a697cae8f3e50c243b2020fe605ef6b9afa5
Steingrimursig <stunisig@gmail.com>
2026-09-15 14:27:58 +0000

    Add SITE_PASSWORD_SHA256 to config.js

--- config.js
+++ config.js
@@ -4,3 +4,4 @@
 // ─────────────────────────────────────────────────────────────
 window.SUPABASE_URL      = "https://lpvyandnghldbdvhzkff.supabase.co";       // t.d. https://abcd1234.supabase.co
 window.SUPABASE_ANON_KEY = "sb_publishable_NN9rjriMcyhLXUEmRs21og_S7ZSn9Tj";          // langur "anon public" lykill
+window.SITE_PASSWORD_SHA256 = "c8c2ccb4a9803cfdbd6eb95a5201e1b3bc3af581de632b1ef2ef0efc7e8c23b8";
